afb360d2224f322b6624c1c7faf56fcb43d5a14a
SNIPL-1 <kjindal123@gmail.com>
2025-10-29 23:29:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Products from './components/Products';
